4941ef506fc4334649b4f1ccc24533736442fd2b
soyeon1351 <pack6411351@gmail.com>
2025-04-17 17:24:34 +0900

    css스타일을 tailwind스타일로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ function App() {
   return (
     <main>
       <Header />
-      <section>
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 py-5 px-10">
         {contents.map((el) => (
-          <Content key={el.id} content={el} />
+        <Content key={el.id} content={el} />
         ))}
       </section>
+
     </main>
   );
 }
